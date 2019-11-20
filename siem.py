1126e92e693fc7cf426dcd218ec5357a96300ba6
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2019-11-20 10:27:41 +0100

    third commit

--- siem.py
+++ siem.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from keras.layers import Dense
 from keras.models import Sequential
@@ -6,7 +5,7 @@ from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-data = pd.read_csv("samples.csv", delimiter=',')
+data = pd.read_csv("dataset.csv", delimiter=',')
 
 labels = data['result']
 
@@ -30,33 +29,38 @@ features['flag'] = flagEncoder.transform(features['flag'])
 labelEncoder = LabelEncoder()
 labelEncoder.fit(labels)
 encoded_Y = labelEncoder.transform(labels)
+print(encoded_Y.shape)
 targets = np_utils.to_categorical(encoded_Y)
 
 model = Sequential()
 model.add(Dense(45, activation='relu', input_dim=features.shape[1]))
 model.add(Dense(45, activation='relu'))
 # softmax => sum =1
-model.add(Dense(4, activation='softmax'))
+model.add(Dense(23, activation='softmax'))
 # adam optimizer => do not use learning rate
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(features, targets, epochs=20)
+model.fit(features, targets, epochs=1)
 
+######"""# %split
 x_train, x_test, y_train, y_test = train_test_split(features, targets, test_size=0.33, random_state=15)
 
-results = model.predict(x_test)
-results = results.round()
-print(results)
+############# predict test split
+# results = model.predict(x_test)
+# results = results.round()
+# print(results)
 
-encoded_results = np.zeros((results.shape[0], 1), int)
-for i in range(results.shape[0]):
-    encoded_results[i] = np.argmax(results[i])
-final_results = labelEncoder.inverse_transform(np.ravel(encoded_results))
-print(final_results)
+###########""# reverse transform
+# encoded_results = np.zeros((results.shape[0], 1), int)
+# for i in range(results.shape[0]):
+#     encoded_results[i] = np.argmax(results[i])
+# final_results = labelEncoder.inverse_transform(np.ravel(encoded_results))
+# print(final_results)
 
+######evaluation
 scores = model.evaluate(x_test, y_test, verbose=0)
 
 print('Accuracy: {}% \n Error: {}'.format(scores[1], 1 - scores[1]))
 
 # save neural networks state
-# model.save('classification_model.h5')
+# model.save('NN.h5')
 # pretrained_model = load_model('classification_model.h5')
