efc0798d352b1b65828bb1adade2f5ba94a5e4cc
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2019-11-27 21:12:39 +0100

    sixth commit

--- siem.py
+++ siem.py
@@ -1,74 +1,89 @@
-import sys
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
 
 import numpy as np
 import pandas as pd
+from elasticsearch import Elasticsearch
 from keras.engine.saving import load_model
 from keras.utils import np_utils
 from sklearn.preprocessing import LabelEncoder
 
+mypath = 'users/'
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+es = Elasticsearch(['http://elastic:changeme@192.168.43.179:9200'])
+lastIndex = 0
 classes = pd.read_csv("classes.csv", delimiter=',')
-samples = pd.read_csv("samples.csv", delimiter=',')
+for file in files:
+    samples = pd.read_csv(mypath + file, delimiter=',')
 
-labels = samples['result']
+    print(files)
 
-features = samples.iloc[:, :-1]
+    labels = samples['result']
 
-# save np.load
-np_load_old = np.load
+    features = samples.iloc[:, :-1]
+    initialRows = samples.iloc[:, :-1]
 
-# modify the default parameters of np.load
-np.load = lambda *a, **k: np_load_old(*a, allow_pickle=True, **k)
+    # save np.load
+    np_load_old = np.load
 
-# encode the protocol type
-protocolTypeEncoder = LabelEncoder()
-protocolTypeEncoder.classes_ = np.load('encodedProtocol.npy')
-features['protocol_type'] = protocolTypeEncoder.transform(features['protocol_type'])
+    # modify the default parameters of np.load
+    np.load = lambda *a, **k: np_load_old(*a, allow_pickle=True, **k)
 
-# service encoder
-serviceEncoder = LabelEncoder()
-serviceEncoder.classes_ = np.load('encodedService.npy')
-features['service'] = serviceEncoder.transform(features['service'])
+    # encode the protocol type
+    protocolTypeEncoder = LabelEncoder()
+    protocolTypeEncoder.classes_ = np.load('encodedProtocol.npy')
+    features['protocol_type'] = protocolTypeEncoder.transform(features['protocol_type'])
 
-# flag encoder
-flagEncoder = LabelEncoder()
-flagEncoder.classes_ = np.load('encodedFlag.npy')
-features['flag'] = flagEncoder.transform(features['flag'])
+    # service encoder
+    serviceEncoder = LabelEncoder()
+    serviceEncoder.classes_ = np.load('encodedService.npy')
+    features['service'] = serviceEncoder.transform(features['service'])
 
-# label encoder
-labelEncoder = LabelEncoder()
-labelEncoder.classes_ = np.load('encodedLabel.npy')
-encoded_Y = labelEncoder.transform(labels)
-print(encoded_Y.shape)
-targets = np_utils.to_categorical(encoded_Y)
+    # flag encoder
+    flagEncoder = LabelEncoder()
+    flagEncoder.classes_ = np.load('encodedFlag.npy')
+    features['flag'] = flagEncoder.transform(features['flag'])
 
-# restore np.load for future normal usage
-np.load = np_load_old
+    # label encoder
+    labelEncoder = LabelEncoder()
+    labelEncoder.classes_ = np.load('encodedLabel.npy')
+    encoded_Y = labelEncoder.transform(labels)
+    print(encoded_Y.shape)
+    targets = np_utils.to_categorical(encoded_Y)
 
-model = load_model('NeuralNetwork.h5')
+    # restore np.load for future normal usage
+    np.load = np_load_old
 
-results = model.predict(features)
-np.set_printoptions(threshold=sys.maxsize)
-print(results.round())
+    model = load_model('NeuralNetwork.h5')
 
-######"""# %split
-# x_train, x_test, y_train, y_test = train_test_split(features, targets, test_size=0.33, random_state=15)
+    results = model.predict(features)
 
-############# predict test split
-# results = model.predict(x_test)
-# results = results.round()
-# print(results)
-#
+    encoded_results = np.zeros((results.shape[0], 1), int)
 
-############ reverse transform
-encoded_results = np.zeros((results.shape[0], 1), int)
-for i in range(results.shape[0]):
-    encoded_results[i] = np.argmax(results[i])
-final_results = labelEncoder.inverse_transform(np.ravel(encoded_results))
-print(final_results)
+    delemiterIndex = file.index('.')
+    userid = file[:delemiterIndex]
+    print('---------------------------------', userid)
+    for i in range(results.shape[0]):
+        encoded_results[i] = np.argmax(results[i])
+        doc = {
+            'userID': userid,
+            'type': labelEncoder.inverse_transform(np.ravel(encoded_results[i]))[0],
+            'body': initialRows.loc[i, :],
+            'timestamp': datetime.now(),
+        }
+        print(labelEncoder.inverse_transform(np.ravel(encoded_results[i])))
+        print(initialRows.loc[i, :])
+        print("---------------------")
+        id_lig = i + lastIndex
+        # posting the new threat to index it at elasticsearch
+        res = es.index(index="threat", doc_type='tweet', id=id_lig, body=doc)
 
-######evaluation
+        print(res['result'])
 
-# print(y_test)
-# scores = model.evaluate(x_test, y_test, verbose=0)
-#
-# print('Accuracy: {}% \n Error: {}'.format(scores[1], 1 - scores[1]))
+        # getting the new values of the threat index
+        res = es.get(index="threat", doc_type='tweet', id=id_lig)
+        print(res['_source'])
+    lastIndex += results.shape[0] + 1
+print(lastIndex)
